1f50a637fac7d283819d82c19e0a1059ab531627
Dogpa <dogpa0130@gmail.com>
2022-05-19 08:18:15 +0800

    Initial Commit

---
+++ Swift Practice 156 Firestore Geopoint/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 156 Firestore Geopoint
+//
+//  Created by Dogpa's MBAir M1 on 2022/5/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 156 Firestore Geopoint/Swift_Practice_156_Firestore_GeopointApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_156_Firestore_GeopointApp.swift
+//  Swift Practice 156 Firestore Geopoint
+//
+//  Created by Dogpa's MBAir M1 on 2022/5/19.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_156_Firestore_GeopointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
